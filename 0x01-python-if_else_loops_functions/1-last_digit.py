d4adcfe01f540541bf50df4fff32efb5616560db
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-11-29 21:37:23 +0100

    task 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str = (f"Last digit of {number:d} is {number:d")
+if number[:-1] > 5:
+    print(str "and is greater than 5")
+elif number[:-1] == 0:
+    print(str "and is 0")
+elif number[:-1] is < 6 && != 0:
+    print(str "and is less than 6 and not 0")
