5e45a57cf68327c028e703b56fee607360951de7
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-12-01 21:43:54 +0100

    Task 0

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: First int
+        b: Secong int
+
+    Returns:
+        The return values, a + b
+    """
+    return (a + b)
