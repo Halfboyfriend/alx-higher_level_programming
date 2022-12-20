3ad26262a69e275bc493fa6eabcedb8d145b3d36
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2022-12-20 21:34:56 +0100

    task-c

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""Represents a Square."""
+
+
+class Square:
+  """Declaring a class Square"""
+  def __init__(self, size = None):
+    """Initialization"""
+    self.__size = size
