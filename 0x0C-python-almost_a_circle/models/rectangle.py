b6a33d9df74364871aa4de3989376bbeb4837ed9
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-01-18 07:28:30 +0100

    task 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines a rectangle class."""
+from models.base import Base
+
+class Rectangle(Base):
+    """Represent a rectangle."""
+
+    """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        """
+    __width = width
+    __height = height
+    __x = x
+    __y = y
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super.__init__()
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+
