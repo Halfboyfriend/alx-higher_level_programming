952f9ef702c374f96a5b99b36dbe64c877322cdc
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-01-18 07:29:22 +0100

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,10 +13,6 @@ class Rectangle(Base):
             y (int): The y coordinate of the new Rectangle.
             id (int): The identity of the new Rectangle.
         """
-    __width = width
-    __height = height
-    __x = x
-    __y = y
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super.__init__()
