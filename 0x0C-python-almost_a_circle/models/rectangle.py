1c7b8d1d0144f2168cb12ad5f132acd57134a778
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-01-18 07:30:34 +0100

    task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -15,10 +15,10 @@ class Rectangle(Base):
         """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super.__init__()
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+        super().__init__(id)
 
 
