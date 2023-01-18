4a7de49b09611f577479051947719a0563d7eac1
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-01-18 07:18:10 +0100

    task 0

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
+"""Defines the base model class."""
+import csv
+import turtle
+import json
+
 class Base:
+    """Represents the model base.
+    Attributes:
+            __nb_objects (int): The number of instantiated Bases
+    """
     __nb_objects = 0
     def __init__(self, id=None):
-        if id != None:
+        if id is not None:
             self.id = id
         else:
-            __nb_objects +=1
+            Base.__nb_objects +=1
+            self.id = Base.__nb_objects
 
 
 
