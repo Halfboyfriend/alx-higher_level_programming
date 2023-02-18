88724b9197319e0d427ddc1079225bc2c4ffc716
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-02-18 04:01:03 +0100

    task-1

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,2 +1,4 @@
 -- creates the table force_name on your MySQL serve
-CREATE TABLE IF NOT EXISTS 'force_name' ('id INT', 'name VARCHAR(256)' NOT NULL);
+CREATE TABLE
+    IF NOT EXISTS 'force_name'
+    ('id INT', 'name VARCHAR(256)' NOT NULL);
