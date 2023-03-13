e9b77e02599348328528e2bf8cba40fa8411c00b
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-03-13 22:22:41 +0100

    task0

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
-let myVar = "C is fun\nPython is cool\nJavaScript is amazing\n";
-console.log(myVar)
+let myVar = "C is fun\n";
+let myVar2 = "Python is cool\n"
+let myVar3 = "JavaScript is amazing\n"
+console.log(myVar,myVar2,myVar3)
