3e6c56ff9823440ea43b34132e2ef35bf845a473
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-03-13 23:07:38 +0100

    task-fill

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/node
-let Char = ['C is fun', 'Python is cool', 'JavaScript is amazing']
-
-console.log(Char[0])
-console.log(Char[1])
-console.log(Char[2])
+for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
+    console.log(line);
+  }
