e940b148848025305e417c8186618dc9b907f5c0
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-03-31 23:45:47 +0100

    networking

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Write a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    arg = "" if len(sys.argv) == 1 else sys.argv[1]
+    load = {"q": arg}
+
+    response = requests.post(url="http://0.0.0.0:5000/search_user", data=arg)
+
+    try:
+        data = response.json()
+        if data == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(data.get("id"), data.get("name")))
+    except ValueError:
+        print('Not a valid JSON')
+
