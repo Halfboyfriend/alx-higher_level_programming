1cc97108f6ac0cc78754bf121077d8de7d18a2c1
Halfboyfriend <owoyemiidrisolamilekan@gmail.com>
2023-03-31 22:56:27 +0100

    networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,10 @@ import urllib.request
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(req) as res:
+    response = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(response) as res:
         body = res.read()
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content:
+        print(f'\t- type: {type(body)}')
+        print(f'\t- content: {body}')
+        print(f'\t- utf8 content: {body.decode("utf-8")}')
